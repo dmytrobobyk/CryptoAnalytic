e085ad5b0e4956e83b76b229f9c7222222dd131b
Dmytro <rs@LVIV-L73151.local>
2022-12-15 16:56:22 +0200

    Removed main fragment, added bottom and drawer navigations, moved all navigation logic into MainActivity, added searchview into toolbar

--- app/src/main/java/com/example/cryptoanalytic/screens/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/cryptoanalytic/screens/calculator/CalculatorFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cryptoanalytic.screens.calculator
 
 import androidx.fragment.app.Fragment
+import com.example.cryptoanalytic.R
 
-class CalculatorFragment : Fragment() {
+class CalculatorFragment : Fragment(R.layout.fragment_calculator) {
 }

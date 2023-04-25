6d5cdbc811e8ff3b959a7ddf5d3b6cb74ff147ed
Dmytro <dimonbobyk@gmail.com>
2023-04-26 02:08:27 +0300

    Removed commented lines

--- data/src/main/java/com/cryptoanalytic/data/di/RepositoriesModule.kt
+++ data/src/main/java/com/cryptoanalytic/data/di/RepositoriesModule.kt
@@ -1,35 +1,13 @@
 package com.cryptoanalytic.data.di
 
-import com.cryptoanalytic.data.datasource.abs.CryptocurrenciesRemoteDataSource
-import com.cryptoanalytic.data.datasource.abs.NewsRemoteDataSource
 import com.cryptoanalytic.data.repository.CryptocurrencyRepositoryImpl
 import com.cryptoanalytic.data.repository.NewsRepositoryImpl
 import com.cryptoanalytic.data.repository.NotificationsRepositoryImpl
-import com.example.database.DaoAggregator
 import com.cryptoanalytic.domain.repository.*
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-
-//@InstallIn(SingletonComponent::class)
-//@Module
-//object RepositoriesModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideCryptocurrencyRepository(remoteDataSource: CryptocurrenciesRemoteDataSource, daoAggregator: DaoAggregator): CryptocurrencyRepository = CryptocurrencyRepositoryImpl(remoteDataSource, daoAggregator)
-//
-//    @Provides
-//    @Singleton
-//    fun provideNewsRepository(remoteDataSource: NewsRemoteDataSource, daoAggregator: DaoAggregator): NewsRepository = NewsRepositoryImpl(remoteDataSource, daoAggregator)
-//
-//    @Provides
-//    @Singleton
-//    fun provideNotificationsRepository(daoAggregator: DaoAggregator): NotificationsRepository = NotificationsRepositoryImpl(daoAggregator)
-//}
 
 @InstallIn(SingletonComponent::class)
 @Module
